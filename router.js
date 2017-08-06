3c5ba79abc7a35fb2b13c60bda38034eeea643e4
cmdrafa <rkraemer01@gmail.com>
2017-08-06 11:09:41 +0100

    Added a new route for user validation on refresh

--- router.js
+++ router.js
@@ -7,8 +7,11 @@ const requireSignin = passport.authenticate('local', { session: false })
 
 module.exports = (app) => {
     app.get('/', requireAuth, (req, res) => {
-        res.send({ hi: 'there' })
+        res.send({ message: 'Super secret code is ABC123' })
     })
     app.post('/signin', requireSignin, Authentication.signin);
     app.post('/signup', Authentication.signup);
+    app.get('/validate', requireAuth, (req, res) => {
+        res.send({ success : true })
+    })
 }
